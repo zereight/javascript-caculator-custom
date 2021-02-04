89924d5dae7cf47b3e3b369db0e78937ed0234ff
zereight <fullstackmachinelearning@gmail.com>
2021-02-04 20:24:01 +0900

    prototype

---
+++ cypress/integration/calculationTest.spec.js
@@ -0,0 +1,73 @@
+const numberClick = (num) => {
+    Array.from(num).forEach(chr => {
+      if ('0' <= chr && chr <= '9') {
+        cy.get('.digit').contains(chr).click();
+      } else {
+        cy.get('.operation').contains(chr).click();
+      }
+    })
+  }
+  
+  const calculationTest = (num1, operator, num2, expectedResult) => {
+    // digit 클래스에 접근해서 7 텍스트 포함하는 것 클릭
+    numberClick(num1);
+    cy.get('.operation').contains(`${operator}`).click();
+    numberClick(num2);
+    cy.get('.operation').contains('=').click();
+    // index.html의 total 접근해서 total의 기댓값이 텍스트 56
+    cy.get('#total').should(($total) => {
+      expect($total).to.contain(`${expectedResult}`);
+      // expect($total).to.contain('59'); // wrong
+    });
+    cy.get('.modifier').click();
+  }
+  
+  describe('사칙연산 테스트', () => {
+    it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
+      cy.visit('http://127.0.0.1:5500/index.html');
+      calculationTest('0', '+', '0', '0');
+      calculationTest('1', '+', '0', '1');
+      calculationTest('999', '+', '999', '1998');
+      calculationTest('055', '+', '45', '100');
+      calculationTest('5000', '+', '87', '587');
+      calculationTest('-11', '+', '10', '-1');
+      calculationTest('X8', '+', '12', '20');
+    });
+  
+    it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
+      cy.visit('http://localhost:5500/index.html');
+      calculationTest('001345', '-', '1', '133');
+      calculationTest('0', '-', '100', '-100');
+      calculationTest('998', '-', '999', '-1');
+      calculationTest('01', '-', '01', '0');
+      calculationTest('9999', '-', '+1', '1000');
+      calculationTest('-0', '-', '1', '-1');
+      calculationTest('-15', '-', '85', '-100');
+      calculationTest('----2', '-', '2', '-4');
+      calculationTest('2', '+', '2-', '4');
+      calculationTest('-0', '+', '2-', '2');
+      calculationTest('2', '-', '2-2-2-2-2', '-8');
+    });
+  
+    it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
+      cy.visit('http://localhost:5500/index.html');
+      calculationTest('7', 'X', '8', '56');
+      calculationTest('00', 'X', '0', '0');
+      calculationTest('999', 'X', '999', '998001');
+      calculationTest('-9999', 'X', '999', '998001');
+      calculationTest('-1', 'X', '09', '-9');
+      calculationTest('-0-0-0-0-0--1', 'X', '09', '-9');
+      calculationTest('00009', 'X', '-09', '0');
+    });
+
+    it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
+      cy.visit('http://localhost:5500/index.html');
+      calculationTest('7', '/', '7', '1');
+      calculationTest('00', '/', '1000', '0');
+      calculationTest('999', '/', '1', '999');
+      calculationTest('-9999', '/', '-999', '1');
+      calculationTest('-1', '/', '10', '0');
+      calculationTest('-0-0-0-0-0--8', '/', '08', '-1');
+      calculationTest('7', '/', '0', '숫자 아님');
+    });
+  })
---
+++ src/js/index.js
@@ -0,0 +1,120 @@
+const acElement = document.querySelector(".modifier");
+const operatorElements = document.querySelectorAll(".operation");
+const digitsElements = document.querySelectorAll(".digit");
+const resultAreaElement = document.getElementById("total");
+
+const firstValue = {
+  value: "0",
+  isEntered: false,
+};
+const operator = {
+  value: "+",
+  isEntered: false,
+};
+const secondValue = {
+  value: "0",
+  isEntered: false,
+};
+let result = "0";
+
+const clear = () => {
+  firstValue.isEntered = secondValue.isEntered = operator.isEntered = false;
+  firstValue.value = secondValue.value = "0";
+  operator.value = "+";
+  result = "0";
+  displayResult(firstValue.value);
+};
+
+const isValidLenght = (numString) => {
+  if (numString.charAt(0) === "-") {
+    if (numString.length - 1 < 3) return true;
+  } else {
+    if (numString.length < 3) return true;
+  }
+  return false;
+};
+
+const displayResult = (num) => (resultAreaElement.innerText = num);
+
+const calculating = () => {
+  if (operator.value === "+") {
+    return parseInt(firstValue.value) + parseInt(secondValue.value);
+  } else if (operator.value === "-") {
+    return parseInt(firstValue.value) - parseInt(secondValue.value);
+  } else if (operator.value === "X") {
+    return parseInt(firstValue.value) * parseInt(secondValue.value);
+  } else if (operator.value === "/") {
+    if (secondValue.value === "0") return "숫자 아님";
+    return parseInt(parseInt(firstValue.value) / parseInt(secondValue.value));
+  }
+};
+
+const firstValueInput = (chr) => {
+  if (!firstValue.isEntered && (("0" <= chr && chr <= "9") || chr === "-")) {
+    firstValue.value = chr;
+    firstValue.isEntered = true;
+    if (chr !== "-") displayResult(firstValue.value);
+  } else if ("0" <= chr && chr <= "9" && isValidLenght(firstValue.value)) {
+    if (firstValue.value.charAt(0) === "-" && chr === "0") return;
+    if (firstValue.value.charAt(0) === "0") {
+      firstValue.value = chr;
+      return;
+    }
+    firstValue.value += chr;
+    firstValue.isEntered = true;
+    displayResult(firstValue.value);
+  }
+};
+
+const operatorValueInput = (chr) => {
+  if (!firstValue.isEntered) firstValueInput(chr);
+  else if (secondValue.isEntered) {
+    const number = calculating();
+    displayResult(number);
+    firstValue.value = number;
+    operator.value = chr;
+    secondValue.value = "0";
+    secondValue.isEntered = false;
+  } else if (!(chr === "=" || firstValue.value === "-")) {
+    operator.value = chr;
+    operator.isEntered = true;
+  }
+};
+
+const secondValueInput = (chr) => {
+  if (secondValue.value.charAt(0) === "0") {
+    secondValue.value = chr;
+  } else if (isValidLenght(secondValue.value)) {
+    secondValue.value += chr;
+  }
+  secondValue.isEntered = true;
+  displayResult(secondValue.value);
+};
+
+const digitsCallback = (e) => {
+  const { target: digitElement } = e;
+  const value = digitElement.innerText;
+  if (!operator.isEntered) {
+    firstValueInput(value);
+  } else {
+    secondValueInput(value);
+  }
+};
+
+const operatorCallback = (e) => {
+  const { target: operatorElement } = e;
+  const value = operatorElement.innerText;
+  operatorValueInput(value);
+};
+
+const init = () => {
+  acElement.addEventListener("click", clear);
+  digitsElements.forEach((digit) =>
+    digit.addEventListener("click", digitsCallback)
+  );
+  operatorElements.forEach((operator) =>
+    operator.addEventListener("click", operatorCallback)
+  );
+};
+
+init();
